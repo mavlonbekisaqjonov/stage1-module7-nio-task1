4412d33fa23d3759c3dbc4fdbd86938b0990d964
Mavl0nbek <mavlonbekisaqjonov222@gmail.com>
2025-02-03 11:15:58 +0500

    made it right

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.util.logging.Logger;
 
 public class FileReader {
+    private static final Logger logger = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder outt = new StringBuilder();
+
+        try (FileInputStream fr =new FileInputStream(file)){
+            int byteData;
+            while ((byteData = fr.read()) != -1) {
+                outt.append((char) byteData);
+            }
+        } catch(IOException e){
+            logger.warning("This is a warning message!");
+        }
+
+        String out = outt.toString();
+
+        String[] lines = out.split("\n");
+        String name = lines[0].split(": ")[1].trim();
+        int age = Integer.parseInt(lines[1].split(": ")[1].trim());
+        String email = lines[2].split(": ")[1].trim();
+        long phone = Long.parseLong(lines[3].split(": ")[1].trim());
+
+        return new Profile(name, age, email, phone);
     }
 }
